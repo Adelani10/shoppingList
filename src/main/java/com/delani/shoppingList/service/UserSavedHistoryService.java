a98a0d380c84b972a42649701ddd75480ad9bca6
ade <harounadelani@gmail.com>
2024-11-08 11:45:32 +0100

    changes

--- src/main/java/com/delani/shoppingList/service/UserSavedHistoryService.java
+++ src/main/java/com/delani/shoppingList/service/UserSavedHistoryService.java
@@ -1,5 +1,6 @@
 package com.delani.shoppingList.service;
 
+import com.delani.shoppingList.model.Item;
 import com.delani.shoppingList.model.User;
 import com.delani.shoppingList.model.UserSavedHistory;
 import com.delani.shoppingList.repo.UserRepository;
@@ -7,6 +8,7 @@ import com.delani.shoppingList.repo.UserSavedHistoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -23,9 +25,11 @@ public class UserSavedHistoryService {
   UserService userService;
 
   public void addToHistory(UserSavedHistory userSavedHistory) {
+    List<Item> list = new ArrayList<>();
     User user = userService.getCurrentUser();
     userSavedHistory.setUserId(user.getUserId());
     user.getSavedList().add(userSavedHistory);
+    user.setCurrentList(list);
     userRepository.save(user);
     userSavedHistoryRepository.save(userSavedHistory);
   }
