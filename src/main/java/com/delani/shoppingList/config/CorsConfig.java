b92d4c3b1fe1da707ba58cbf555d0dcbc53031c2
ade <harounadelani@gmail.com>
2024-11-12 01:42:45 +0100

    cors

---
+++ src/main/java/com/delani/shoppingList/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.delani.shoppingList.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.CorsConfigurationSource;
+import java.util.Arrays;
+
+@Configuration
+public class CorsConfig {
+
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:3001", "https://shoppinglist-yw62.onrender.com", "https://shopping-list-fe-nu.vercel.app"));
+    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+    configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+    configuration.setAllowCredentials(true);
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
+  }
+}
--- src/main/java/com/delani/shoppingList/config/SecurityConfig.java
+++ src/main/java/com/delani/shoppingList/config/SecurityConfig.java
@@ -17,6 +17,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -33,8 +34,10 @@ public class SecurityConfig {
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
     httpSecurity
         .csrf(customizer -> customizer.disable())
+        .cors(Customizer.withDefaults())
         .authorizeHttpRequests(request -> request.requestMatchers("/api/v1/user/register", "/api/v1/user/login")
             .permitAll()
             .anyRequest()
